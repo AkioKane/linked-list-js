06d5c0bb2676f7bb0e0b8616d5ec6e6dba1fd2c7
Akio <aakocherga@mail.ru>
2024-08-29 00:31:43 +0700

    Created classes

---
+++ index.js
@@ -0,0 +1,12 @@
+class LinkedList {
+  constructor() {
+    // values
+  }
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
